c09e2d4b858b245f3ce75da14181a7b4c3c9c550
Velenoks <pavel.zakharov.1994@yandex.ru>
2021-03-04 20:56:33 +0400

    start project and add models

---
+++ calculator/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import *
+
+
+admin.site.register(Customers)
+admin.site.register(Estimate)
+admin.site.register(States)
+admin.site.register(Materials)
+admin.site.register(MeasurementUnits)
+admin.site.register(MaterialsType)
+admin.site.register(PriceLists)
+admin.site.register(BoxСalculations)
+admin.site.register(FoundationСalculations)
+admin.site.register(RoofCalculations)
+
