0f7865e945376781525375258f618b1d85bd4407
Artem Ovchinnikov <bd@mail.ru>
2021-03-16 19:23:04 +0400

    add information for authUser

--- calculator/views.py
+++ calculator/views.py
@@ -25,6 +25,7 @@ class FrameOpeningsViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     queryset = FrameOpening.objects.all()
     serializer_class = FrameSerializer
 
+
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
