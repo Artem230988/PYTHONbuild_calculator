a25ea9df32c8579a14cb4768a887b8194ca7fb53
Velenoks <pavel.zakharov.1994@yandex.ru>
2021-03-27 15:29:27 +0400

    change view for post calculation

--- config/settings.py
+++ config/settings.py
@@ -4,26 +4,26 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECRET_KEY = '*9dwht^)&od&urxjy4-qw=16&+5_wj_^m$7p(son2^!k1ko8j)'
-#
-# DEBUG = True
-#
-# ALLOWED_HOSTS = [
-#     "localhost",
-#     "127.0.0.1",
-#     "[::1]",
-#     "testserver",
-# ]
+SECRET_KEY = '*9dwht^)&od&urxjy4-qw=16&+5_wj_^m$7p(son2^!k1ko8j)'
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+DEBUG = True
 
-DEBUG = int(os.environ.get("DEBUG", default=0))
-
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
-
-INTERNAL_IPS = [
-    '127.0.0.1',
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "[::1]",
+    "testserver",
 ]
+
+# SECRET_KEY = os.environ.get("SECRET_KEY")
+#
+# DEBUG = int(os.environ.get("DEBUG", default=0))
+#
+# ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+#
+# INTERNAL_IPS = [
+#     '127.0.0.1',
+# ]
 
 INSTALLED_APPS = [
     # apps django
@@ -78,23 +78,23 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
-        "USER": os.environ.get("SQL_USER", "user"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
-        "HOST": os.environ.get("SQL_HOST", "localhost"),
-        "PORT": os.environ.get("SQL_PORT", "5432"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+#         "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+#         "USER": os.environ.get("SQL_USER", "user"),
+#         "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+#         "HOST": os.environ.get("SQL_HOST", "localhost"),
+#         "PORT": os.environ.get("SQL_PORT", "5432"),
+#     }
+# }
 
 
 AUTH_PASSWORD_VALIDATORS = [
